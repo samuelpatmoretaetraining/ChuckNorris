4396d03547adda92cdc8513884e8fbf45db812eb
Vaguehorizon <Vaguehorizon@users.noreply.github.com>
2017-11-22 16:11:49 +0000

    Cache will only update from Web Service once every 5 minutes.

--- app/src/main/java/com/muelpatmore/chucknorris/MainActivity.java
+++ app/src/main/java/com/muelpatmore/chucknorris/MainActivity.java
@@ -1,17 +1,21 @@
 package com.muelpatmore.chucknorris;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.muelpatmore.chucknorris.models.ChuckModel;
 import com.muelpatmore.chucknorris.models.JokeModel;
 import com.muelpatmore.chucknorris.services.RequestInterface;
 import com.muelpatmore.chucknorris.services.ServerConnection;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -25,12 +29,47 @@ public class MainActivity extends AppCompatActivity {
     private RequestInterface requestInterface;
     private ArrayList<JokeModel> jokes;
     private RecyclerView recyclerView;
+    private JokeAdapter jokeAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private Date date = new Date();
+    private long cacheTimestamp = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+
+        /*
+         * Sets up a SwipeRefreshLayout.OnRefreshListener that is invoked when the user
+         * performs a swipe-to-refresh gesture.
+         */
+        swipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        Log.i(TAG, "onRefresh called from SwipeRefreshLayout");
+
+                        // This method performs the actual data-refresh operation.
+                        // The method calls setRefreshing(false) when it's finished.
+                        updateJokes();
+                    }
+                }
+        );
+
         updateCache();
+        cacheTimestamp = date.getTime();
+        Log.i(TAG, "Current time: "+cacheTimestamp);
+    }
+
+    /**
+     *
+     */
+    private void updateJokeList() {
+        Log.i(TAG, "View Refreshed");
+        jokeAdapter.notifyDataSetChanged();
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     private void updateCache() {
@@ -65,8 +104,18 @@ public class MainActivity extends AppCompatActivity {
     private void initRecyclerView() {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerLayout);
         recyclerView.setLayoutManager(new LinearLayoutManager((getApplicationContext())));
-        recyclerView.setAdapter((new JokeAdapter(jokes, R.layout.joke_card, getApplicationContext())));
+        jokeAdapter = new JokeAdapter(jokes, R.layout.joke_card, getApplicationContext());
+        recyclerView.setAdapter(jokeAdapter);
     }
 
+    private void updateJokes() {
+        long timeElapsed = (System.currentTimeMillis() - cacheTimestamp);
+        Toast.makeText(this, timeElapsed + "ms since last update", Toast.LENGTH_SHORT).show();
+        if(timeElapsed > 300000) {
+            updateCache();
+            cacheTimestamp = System.currentTimeMillis();
+        }
+        updateJokeList();
+    }
 
 }
